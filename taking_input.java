d7fa44e9ea6ab1924c96730d9fd891e4f6dc6414
NishanChy733689 <155312200+NishanChy733689@users.noreply.github.com>
2024-09-21 13:51:03 +0600

    change

--- taking_input.java
+++ taking_input.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class taking_input {
+    public static void main(String[] args){
+        Scanner inp=new Scanner(System.in);
+        System.out.println("Enter some text");
+        String text=inp.nextLine();
+        System.out.println("You wrote :\n"+text);
+    }
 }
